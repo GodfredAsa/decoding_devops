7c817b9cdb362339c877d12c34aa21316fd76543
GodfredAsa <godfred.asumadu.asamoah@gmail.com>
2023-12-05 14:40:25 +0000

    docker guides

---
+++ docker-engine/steps.sh
@@ -0,0 +1,9 @@
+# Local set guide 
+vagrant init ubuntu/bionic64
+vagrant up 
+vagrant ssh 
+
+# install docker ffing docker docs guide then check the docker version installed
+# </> docker --version 
+# give vagrant user docker command privileges, adding vagrant user into docker groups 
+# </> sudo usermod -aG docker vagrant 
